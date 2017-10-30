ba3bf03b80f70b99e5d65ba175ed8d2ae9a8db7d
ttian <tiant@qualtrics.com>
2017-10-30 15:16:18 -0700

    tune lstm and add bilstm

--- bilstm.py
+++ bilstm.py
@@ -4,28 +4,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class LSTMSentiment(nn.Module):
+class BiLSTMSentiment(nn.Module):
 
     def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size, use_gpu, batch_size, dropout=0.5):
-        super(LSTMSentiment, self).__init__()
+        super(BiLSTMSentiment, self).__init__()
         self.hidden_dim = hidden_dim
         self.use_gpu = use_gpu
         self.batch_size = batch_size
         self.dropout = dropout
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim)
-        self.hidden2label = nn.Linear(hidden_dim, label_size)
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=True)
+        self.hidden2label = nn.Linear(hidden_dim*2, label_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
         # first is the hidden h
         # second is the cell c
         if self.use_gpu:
-            return (Variable(torch.zeros(1, self.batch_size, self.hidden_dim).cuda()),
-                    Variable(torch.zeros(1, self.batch_size, self.hidden_dim).cuda()))
+            return (Variable(torch.zeros(2, self.batch_size, self.hidden_dim).cuda()),
+                    Variable(torch.zeros(2, self.batch_size, self.hidden_dim).cuda()))
         else:
-            return (Variable(torch.zeros(1, self.batch_size, self.hidden_dim)),
-                    Variable(torch.zeros(1, self.batch_size, self.hidden_dim)))
+            return (Variable(torch.zeros(2, self.batch_size, self.hidden_dim)),
+                    Variable(torch.zeros(2, self.batch_size, self.hidden_dim)))
 
     def forward(self, sentence):
         x = self.embeddings(sentence).view(len(sentence), self.batch_size, -1)
--- train_batch.py
+++ train_batch.py
@@ -3,15 +3,41 @@ import torch.nn as nn
 from torch import optim
 import time, random
 import os
-from data_helper import load_data
 from tqdm import tqdm
 from lstm import LSTMSentiment
-import data_helper
-import torchtext.datasets as datasets
+from bilstm import BiLSTMSentiment
 from torchtext import data
+import numpy as np
+import argparse
+
 torch.set_num_threads(8)
 torch.manual_seed(1)
 random.seed(1)
+
+
+def load_bin_vec(fname, vocab):
+    """
+    Loads 300x1 word vecs from Google (Mikolov) word2vec
+    """
+    word_vecs = {}
+    with open(fname, "rb") as f:
+        header = f.readline()
+        vocab_size, layer1_size = map(int, header.split())
+        binary_len = np.dtype('float32').itemsize * layer1_size
+        for line in range(vocab_size):
+            word = []
+            while True:
+                ch = f.read(1).decode('latin-1')
+                if ch == ' ':
+                    word = ''.join(word)
+                    break
+                if ch != '\n':
+                    word.append(ch)
+            if word in vocab:
+               word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
+            else:
+                f.read(binary_len)
+    return word_vecs
 
 
 def get_accuracy(truth, pred):
@@ -29,8 +55,7 @@ def train_epoch_progress(model, train_iter, loss_function, optimizer, text_field
     truth_res = []
     pred_res = []
     count = 0
-    acc = 0
-    for batch in tqdm(train_iter):
+    for batch in tqdm(train_iter, desc='Train epoch '+str(epoch+1)):
         sent, label = batch.text, batch.label
         label.data.sub_(1)
         truth_res += list(label.data)
@@ -98,7 +123,29 @@ def evaluate(model, data, loss_function, name):
     return acc
 
 
-EPOCHS = 10
+def load_sst(text_field, label_field, batch_size):
+    train, dev, test = data.TabularDataset.splits(path='./data/SST2/', train='train.tsv',
+                                                  validation='dev.tsv', test='test.tsv', format='tsv',
+                                                  fields=[('text', text_field), ('label', label_field)])
+    text_field.build_vocab(train, dev, test)
+    label_field.build_vocab(train, dev, test)
+    train_iter, dev_iter, test_iter = data.BucketIterator.splits((train, dev, test),
+                batch_sizes=(batch_size, len(dev), len(test)), sort_key=lambda x: len(x.text), repeat=False, device=-1)
+    return train_iter, dev_iter, test_iter
+
+
+# def adjust_learning_rate(learning_rate, optimizer, epoch):
+#     lr = learning_rate * (0.1 ** (epoch // 10))
+#     for param_group in optimizer.param_groups:
+#         param_group['lr'] = lr
+#     return optimizer
+
+
+args = argparse.ArgumentParser()
+args.add_argument('--m', dest='model', default='lstm', help='specify the mode to use (default: lstm)')
+args = args.parse_args()
+
+EPOCHS = 20
 USE_GPU = torch.cuda.is_available()
 EMBEDDING_DIM = 300
 HIDDEN_DIM = 150
@@ -108,46 +155,61 @@ timestamp = str(int(time.time()))
 best_dev_acc = 0.0
 
 
-def load_sst(text_field, label_field, batch_size):
-    train, dev, test = data.TabularDataset.splits(path='./data/SST2/', train='train.tsv',
-                                                  validation='dev.tsv', test='test.tsv', format='tsv',
-                                                  fields=[('text', text_field), ('label', label_field)])
-    text_field.build_vocab(train, dev, test)
-    label_field.build_vocab(train, dev, test)
-    train_iter, dev_iter, test_iter = data.BucketIterator.splits((train, dev, test),\
-        batch_sizes=(batch_size, len(dev), len(test)), sort_key=lambda x: len(x.text), repeat=False, device=-1)
-    return train_iter, dev_iter, test_iter
-
-
 text_field = data.Field(lower=True)
 label_field = data.Field(sequential=False)
 train_iter, dev_iter, test_iter = load_sst(text_field, label_field, BATCH_SIZE)
 
-# text_field.vocab.load_vectors(wv_type='', wv_dim=300)
-model = LSTMSentiment(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab), label_size=len(label_field.vocab)-1,\
-                      use_gpu=USE_GPU, batch_size=BATCH_SIZE)
+if not args.model:
+    model = LSTMSentiment(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab), label_size=len(label_field.vocab)-1,\
+                          use_gpu=USE_GPU, batch_size=BATCH_SIZE)
+
+if args.model == 'bilstm':
+    model = BiLSTMSentiment(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab), label_size=len(label_field.vocab)-1,\
+                          use_gpu=USE_GPU, batch_size=BATCH_SIZE)
+
 if USE_GPU:
     model = model.cuda()
 
-# model.embeddings.weight.data.copy_(torch.from_numpy(pretrained_embeddings))
+
+print('Load word embeddings...')
+# # glove
+# text_field.vocab.load_vectors('glove.6B.100d')
+
+# word2vector
+word_to_idx = text_field.vocab.stoi
+pretrained_embeddings = np.random.uniform(-0.25, 0.25, (len(text_field.vocab), 300))
+pretrained_embeddings[0] = 0
+word2vec = load_bin_vec('./data/GoogleNews-vectors-negative300.bin', word_to_idx)
+for word, vector in word2vec.items():
+    pretrained_embeddings[word_to_idx[word]-1] = vector
+
+# text_field.vocab.load_vectors(wv_type='', wv_dim=300)
+
+model.embeddings.weight.data.copy_(torch.from_numpy(pretrained_embeddings))
+# model.embeddings.weight.data = text_field.vocab.vectors
+# model.embeddings.embed.weight.requires_grad = False
+
 
 best_model = model
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 loss_function = nn.NLLLoss()
 
 print('Training...')
+out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+print("Writing to {}\n".format(out_dir))
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 for epoch in range(EPOCHS):
     avg_loss, acc = train_epoch_progress(model, train_iter, loss_function, optimizer, text_field, label_field, epoch)
     tqdm.write('Train: loss %.2f acc %.1f' % (avg_loss, acc*100))
     dev_acc = evaluate(model, dev_iter, loss_function, 'Dev')
     if dev_acc > best_dev_acc:
+        if best_dev_acc > 0:
+            os.system('rm '+ out_dir + '/best_model' + '.pth')
         best_dev_acc = dev_acc
         best_model = model
-test_acc = evaluate(best_model, test_iter, loss_function,'Test')
+        torch.save(best_model.state_dict(), out_dir + '/best_model' + '.pth')
+        # evaluate on test with the best dev performance model
+        test_acc = evaluate(best_model, test_iter, loss_function, 'Test')
+test_acc = evaluate(best_model, test_iter, loss_function, 'Final Test')
 
-
-out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-print("Writing to {}\n".format(out_dir))
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-torch.save(best_model.state_dict(), out_dir + '/best_model' + '.pth')
