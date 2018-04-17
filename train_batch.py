457ffd83de90a9d6d472d8ffc31bbffe40a8b236
Tian Tian <tian.tianclaire@gmail.com>
2018-04-16 21:20:58 -0700

    Update train_batch.py

--- train_batch.py
+++ train_batch.py
@@ -131,6 +131,9 @@ def load_sst(text_field, label_field, batch_size):
     label_field.build_vocab(train, dev, test)
     train_iter, dev_iter, test_iter = data.BucketIterator.splits((train, dev, test),
                 batch_sizes=(batch_size, len(dev), len(test)), sort_key=lambda x: len(x.text), repeat=False, device=-1)
+    ## for GPU run
+#     train_iter, dev_iter, test_iter = data.BucketIterator.splits((train, dev, test),
+#                 batch_sizes=(batch_size, len(dev), len(test)), sort_key=lambda x: len(x.text), repeat=False, device=None)
     return train_iter, dev_iter, test_iter
 
 
